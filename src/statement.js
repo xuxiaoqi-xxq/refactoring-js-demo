aa05e3e9eb6ad4e32425bb2e3ee077e6070bbca2
Eva Xu <xuev6@oocl.com>
2020-08-27 14:54:53 +0800

    [eva] refactor: extract swith statement to calculateAmount method

--- src/statement.js
+++ src/statement.js
@@ -4,24 +4,7 @@ function statement (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = calculateAmount(play, perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -40,6 +23,28 @@ const format = new Intl.NumberFormat('en-US', {
         currency: 'USD',
         minimumFractionDigits: 2,
       }).format;
+
+function calculateAmount(play, perf) {
+    let thisAmount = 0;
+    switch (play.type) {
+          case 'tragedy':
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+              thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+          case 'comedy':
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+              thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+          default:
+            throw new Error(`unknown type: ${play.type}`);
+        }
+    return thisAmount;
+}
 
 module.exports = {
   statement,
