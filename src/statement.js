fd5d4c0dbd8ba936688f6e728e689c5d0250ecb1
Eva Xu <xuev6@oocl.com>
2020-08-27 14:49:35 +0800

    [eva] refactor: extract format

--- src/statement.js
+++ src/statement.js
@@ -2,11 +2,6 @@ function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -39,6 +34,12 @@ function statement (invoice, plays) {
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 }
+
+const format = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      }).format;
 
 module.exports = {
   statement,
